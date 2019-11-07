1c9ec0ada75cc1d42fc5563603a82e7111e1458a
marciano <mcnvrodrigues@gmail.com>
2019-11-07 20:52:22 -0300

    final

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 
 require('dotenv').config();
+
 const session    = require("express-session");
 const MongoStore = require("connect-mongo")(session);
 
@@ -17,7 +18,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
-const router = require('./routes/auth');
+
+
+// const router = require('./routes/auth');
 
 const mongoose = require('mongoose');
 // const userModel = require('./models');
@@ -27,8 +30,20 @@ mongoose.connect('mongodb://localhost/basicAuth', { userNewParser: true})
         console.log('Connected to Mongo!');
     }).catch(err => {
         console.error('Error connecting to mongo', err);
-    });
+});
 
-app.use('/', router);
+app.use(session({
+    secret: "basic-auth-secret",
+    cookie: { maxAge: 60000 },
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60 // 1 day
+    })
+}));
+
+// app.use('/', router);
+
+app.use('/', require('./routes/auth'));
+app.use('/', require('./routes/site-routes'));
 
 app.listen(port, () => console.log("My bcrypt project is running on port 3000"));
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -11,6 +11,10 @@ router.get("/signup", (req, res, next) => {
     // res.send("auth/signup");
 });
 
+router.get("/login", (req, res, next) => {
+  res.render("auth/login");
+});
+
 router.post("/signup", (req, res, next) => {
     const username = req.body.username;
     const password = req.body.password;
@@ -27,7 +31,7 @@ router.post("/signup", (req, res, next) => {
       User.findOne({ "username": username })
       .then(user => {
         if (user !== null) {
-            res.render("auth/signup", {
+            res.render("auth/login", {
               errorMessage: "The username already exists!"
             });
             return;
@@ -52,6 +56,48 @@ router.post("/signup", (req, res, next) => {
       })
   
     
+  });
+
+
+  router.post("/login", (req, res, next) => {
+    const theUsername = req.body.username;
+    const thePassword = req.body.password;
+  
+    if (theUsername === "" || thePassword === "") {
+      res.render("auth/login", {
+        errorMessage: "Please enter both, username and password to sign up."
+      });
+      return;
+    }
+  
+    User.findOne({ "username": theUsername })
+    .then(user => {
+        if (!user) {
+          res.render("auth/login", {
+            errorMessage: "The username doesn't exist."
+          });
+          return;
+        }
+        if (bcryptjs.compareSync(thePassword, user.password)) {
+          // Save the login in the session!
+          req.session.currentUser = user;
+          res.redirect("/");
+        } else {
+          res.render("auth/login", {
+            errorMessage: "Incorrect password"
+          });
+        }
+    })
+    .catch(error => {
+      next(error);
+    })
+  });
+
+  router.get("/logout", (req, res, next) => {
+    req.session.destroy((err) => {
+      // cannot access session here
+      res.redirect("/login");
+    });
   });
   
 
